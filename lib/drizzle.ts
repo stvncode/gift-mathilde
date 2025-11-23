8b1723d6e4a6dbfac486279ae72f680b204ae743
stvncode <steven.travers20@gmail.com>
2025-11-23 13:08:42 +0100

    Initialize

---
+++ lib/drizzle.ts
@@ -0,0 +1,39 @@
+import {
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  uniqueIndex,
+} from 'drizzle-orm/pg-core'
+import { InferSelectModel, InferInsertModel } from 'drizzle-orm'
+import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
+
+const sql = postgres(process.env.POSTGRES_URL!, {
+  ssl: 'require',
+  max: 1,
+  idle_timeout: 20,
+  max_lifetime: 60 * 30,
+})
+
+export const UsersTable = pgTable(
+  'profiles',
+  {
+    id: serial('id').primaryKey(),
+    name: text('name').notNull(),
+    email: text('email').notNull(),
+    image: text('image').notNull(),
+    createdAt: timestamp('createdAt').defaultNow().notNull(),
+  },
+  (users) => {
+    return {
+      uniqueIdx: uniqueIndex('unique_idx').on(users.email),
+    }
+  }
+)
+
+export type User = InferSelectModel<typeof UsersTable>
+export type NewUser = InferInsertModel<typeof UsersTable>
+
+// Connect to  Postgres
+export const db = drizzle(sql)
